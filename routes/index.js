bca5bc2c186fed28d0b3635bea97725a1fb44377
Laurenne <laurenneoliver@gmail.com>
2025-02-21 14:58:28 -0600

    Setting up the gateway API

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+router.all('/:apiName', (req, res) => {
+    console.log(req.params.apiName);
+    res.send(req.params.apiName+ '\n');
+}); //endpoint
+
+module.exports = router;
