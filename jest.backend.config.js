257d9a74a675909a88566190502b842f151828a1
Laurenne <laurenneoliver@gmail.com>
2025-02-24 12:41:09 -0600

    Add test are added and passed

--- jest.backend.config.js
+++ jest.backend.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-    testEnvironment: "node",  // Run backend tests in a Node.js environment
-    testMatch: ["<rootDir>/server/**/*.test.js"],  // Only run backend test files
-    setupFilesAfterEnv: ["<rootDir>/server/testSetup.js"], // Optional setup
-};
+    testEnvironment: "node",
+    testMatch: ["<rootDir>/server.test.js"], // Adjust this if the file is elsewhere
+  };
   
